6950d17f2ad54c5381253631c153162c79b652d0
Michael Foley <michaeldfoley@gmail.com>
2015-04-15 14:53:49 -0400

    added git remote to buildcontrol

--- Gruntfile.js
+++ Gruntfile.js
@@ -378,7 +378,7 @@ module.exports = function (grunt) {
     buildcontrol: {
       dist: {
         options: {
-          remote: '../',
+          remote: 'git@github.com:michaeldfoley/wedding.git',
           branch: 'gh-pages',
           commit: true,
           push: true
