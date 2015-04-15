5914d95106e3de16e97eee69e7d467b18c442cf7
Michael Foley <michaeldfoley@gmail.com>
2015-04-15 11:42:43 -0400

    fix missing ; in lazyload settings

--- app/_src/lazyload.js
+++ app/_src/lazyload.js
@@ -2,12 +2,12 @@
 // 
 // --------------------------------------------------
 
-window.lazySizesConfig = window.lazySizesConfig || {}
-window.lazySizesConfig.lazyClass = 'js-lazyload'
-lazySizesConfig.preloadClass = 'js-lazypreload'
-lazySizesConfig.loadingClass = 'js-lazyloading'
-lazySizesConfig.loadedClass = 'js-lazyloaded'
-window.lazySizesConfig.preloadAfterLoad = !/mobi/i.test(navigator.userAgent)
+window.lazySizesConfig = window.lazySizesConfig || {};
+window.lazySizesConfig.lazyClass = 'js-lazyload';
+lazySizesConfig.preloadClass = 'js-lazypreload';
+lazySizesConfig.loadingClass = 'js-lazyloading';
+lazySizesConfig.loadedClass = 'js-lazyloaded';
+window.lazySizesConfig.preloadAfterLoad = !/mobi/i.test(navigator.userAgent);
 
 
 // Lazysizes noscript/progressive enhancement extension
