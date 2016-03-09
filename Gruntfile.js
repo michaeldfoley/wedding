e839c832d1d5083e14b931ceb4595610079fa453
Michael Foley <michaeldfoley@gmail.com>
2016-03-09 17:03:58 -0500

    remove cors headers from test server

--- Gruntfile.js
+++ Gruntfile.js
@@ -84,12 +84,7 @@ module.exports = function (grunt) {
               '.jekyll',
               '.tmp',
               '<%= yeoman.app %>'
-            ],
-            middleware: function (req, res, next) {
-              console.log('Adding CORS header for ' + req.method + ': ' + req.url);
-              res.setHeader('Access-Control-Allow-Origin', '*');
-              next();
-            }
+            ]
           },
           port: 3000,
           watchTask: true
